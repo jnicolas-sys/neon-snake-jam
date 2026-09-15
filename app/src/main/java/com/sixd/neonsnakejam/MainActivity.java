245590cbde97fd8a2b030799d92b2c81b3e5ba2b
jnicolas-sys <jnicolas@6d-ingenieros.com>
2026-09-15 23:02:43 +0200

    Build Neon Snake Jam Android APK

---
+++ app/src/main/java/com/sixd/neonsnakejam/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sixd.neonsnakejam;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override public void onBackPressed() {
+        webView.evaluateJavascript("(document.getElementById('game').classList.contains('on')||document.getElementById('records').classList.contains('on')) ? (goHome(),'HOME') : 'EXIT'", value -> {
+            if ("\"EXIT\"".equals(value)) super.onBackPressed();
+        });
+    }
+}
